5f77ea74d9250161b2ea3a45ca43e8e9d200f269
Walmyr <wlsf82@gmail.com>
2021-12-13 22:22:42 +0100

    Simplify tests not using custom commands

--- cypress/integration/loginProgrammatically.spec.js
+++ cypress/integration/loginProgrammatically.spec.js
@@ -1,7 +1,16 @@
 it('successfully logs in programmatically', () => {
   cy.intercept('GET', `${Cypress.env('apiUrl')}/models?userId=*`).as('getUserModels')
-  cy.loginViaAPI()
+
+  cy.request('POST', `${Cypress.env('apiUrl')}/users/login`, {
+    username: Cypress.env('userEmail'),
+    password: Cypress.env('userPassword'),
+  }).then((response) => {
+    cy.setCookie('sessionId', response.body.sessionId)
+    cy.setCookie('userId', response.body.userId)
+    cy.setCookie('userName', response.body.userName)
+  })
+
+  cy.visit('/#!/main')
   cy.wait('@getUserModels')
-  cy.url().should('be.equal', `${Cypress.config('baseUrl')}/#!/main`)
   cy.contains('h2', 'Models').should('be.visible')
 })
--- cypress/integration/loginViaGUI.spec.js
+++ cypress/integration/loginViaGUI.spec.js
@@ -1,6 +1,11 @@
 it('successfully logs in via GUI', () => {
   cy.intercept('GET', `${Cypress.env('apiUrl')}/models?userId=*`).as('getUserModels')
-  cy.loginViaGUI()
+
+  cy.visit('/')
+  cy.get('#userEmail').type(Cypress.env('userEmail'))
+  cy.get('#userPassword').type(Cypress.env('userPassword'), { log: false })
+  cy.contains('button', 'Login').click()
+
   cy.wait('@getUserModels')
   cy.url().should('be.equal', `${Cypress.config('baseUrl')}/#!/main`)
   cy.contains('h2', 'Models').should('be.visible')
